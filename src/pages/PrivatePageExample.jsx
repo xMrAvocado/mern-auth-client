5570348f49307737224d2b3425e1e165d22bb0d2
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-08-04 17:18:51 +0200

    added username to signup

--- src/pages/PrivatePageExample.jsx
+++ src/pages/PrivatePageExample.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
-function PrivateExample() {
+function PrivatePageExample() {
+
+  const [dataOnlyForLoggedUsers, setData] = useState(null)
+
+  useEffect(() => {
+    getData()
+  })
+
+  const getData = async () => {
+    try {
+      
+      // call a private route here...
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // loading handler here
+
   return (
     <div>
       
       <h3>Ejemplo de página privada</h3>
-      <p>Solo usuarios que hayan validado credenciales deberian poder acceder</p>
+      <p>Solo usuarios que hayan validado credenciales deberian poder acceder y ver la siguiente información:</p>
 
     </div>
   )
 }
 
-export default PrivateExample
+export default PrivatePageExample
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 function Signup() {
 
   const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleEmailChange = (e) => setEmail(e.target.value);
+  const handleUsernameChange = (e) => setUsername(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
 
   const handleSignup = async (e) => {
@@ -27,6 +29,16 @@ function Signup() {
           name="email"
           value={email}
           onChange={handleEmailChange}
+        />
+
+        <br />
+
+        <label>Username:</label>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleUsernameChange}
         />
 
         <br />
